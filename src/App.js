944bafa986509058d4d98bf50d77052befdba47d
Steve Hayes <stevehayesdigital@gmail.com>
2023-03-15 09:52:30 -0400

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,55 +1,33 @@
-import { ThemeProvider, BaseStyles, Link } from '@primer/react'
-import Layout from './layouts/Layout'
+import { ThemeProvider, BaseStyles, UnderlineNav } from '@primer/react'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import ColorModeSwitcher from './components/ColorModeSwitcher'
-import Banner from './components/Banner'
+import Banners from './pages/Banners'
 
 function App() {
-    const types = [
-        {
-            type: 'default',
-            title: 'This is a default banner',
-            action: false,
-        },
-        {
-            type: 'accent',
-            title: 'This is an accent banner',
-            action: true,
-        },
-        {
-            type: 'attention',
-            title: 'This is an attention banner',
-            action: true,
-        },
-        {
-            type: 'danger',
-            title: 'This is a danger banner',
-            action: true,
-        },
-        {
-            type: 'done',
-            title: 'This is a done banner',
-            action: true,
-        },
-    ]
+    const location = useLocation().pathname
+
+    function checkRoute(path) {
+        return location === path ? true : false
+    }
+
     return (
         <ThemeProvider colorMode="light">
             <BaseStyles>
                 <ColorModeSwitcher />
-                <Layout>
-                    {types.map((type) => (
-                        <Banner
-                            // One of: default, accent, attention, danger, done
-                            type={type.type}
-                            // One of: false, true
-                            action={type.action}
-                            // String
-                            title={type.title}
-                        >
-                            Lorem ipsum <Link>dolor sit amet</Link>, consectetur
-                            adipiscing elit.
-                        </Banner>
-                    ))}
-                </Layout>
+                <UnderlineNav>
+                    <UnderlineNav.Link href="/" selected={checkRoute('/')}>
+                        Banners
+                    </UnderlineNav.Link>
+                    <UnderlineNav.Link
+                        href="/banners"
+                        selected={checkRoute('/banners')}
+                    >
+                        Next
+                    </UnderlineNav.Link>
+                </UnderlineNav>
+                <Routes>
+                    <Route path="/" element={<Banners />} />
+                </Routes>
             </BaseStyles>
         </ThemeProvider>
     )
